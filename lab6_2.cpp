7c0001305b1d1d5f448468f90e9d58512167d66e
romsaejao <76035596+romsaejao@users.noreply.github.com>
2020-12-25 18:05:23 +0700

    updated

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,66 @@
-//Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string n,mv,d,w;
+	long int id;
+	
+	
+	cout<<"Fahsai: Sawadee ka...Can you tell me your name?"<<"\n";
+	cout<<"?????: ";
+	getline(cin,n);
+	cout<<"Fahsai: Wow!!! "<<n << " is a really cool name."<< "\n";
+	cout<<"Fahsai: I think you are an Engineering student. What is your student ID?"<< "\n";
+	cout<< n << ": ";
+	cin>>id;
+	cin.ignore();
+	id = id/10000000;
+	id = id - 12;
+	
+	cout<<"Fahsai: I think you may be GEAR "<< id <<". I have a free movie ticket for you."<< "\n";
+	cout<<"Fahsai: Let's go to the cinema together!!!"<<"\n";
+	cout<<"Fahsai: What movie do you want to watch?"<<"\n";
+	cout<< n <<": ";
+	getline(cin,mv);
+	cout<<"Fahsai: So....which day are you free to go with me?"<<"\n";
+	cout<< n <<": ";
+	getline(cin,d);
+	cout<<"Fahsai: "<< d <<"....that is OK!!! I'm looking forward to watching " << mv << " with you."<<"\n";
+	cout<< n <<": ";
+	getline(cin,w);
+	cout<<"Fahsai: 555+ see you "<< d << ". Bye Bye "<<"\\(^ ^)/";
+	
+	
+	
+}
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
